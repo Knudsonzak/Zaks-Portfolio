00c453d2c1941643931f8ced4238459472d8911e
Knudsonzak <zakknudson6@gmail.com>
2026-02-03 15:18:20 -0500

    port change

--- server.js
+++ server.js
@@ -16,14 +16,11 @@ app.use(express.static(path.join(__dirname)));
 // Configure nodemailer transporter
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: true
     },
     logger: true, // Enable logging
     debug: true // Include debug info
